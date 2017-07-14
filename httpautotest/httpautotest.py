55edd8a90f8e1579b88638a4f0bd55ac0fd5f2c0
r455678 <717577064@qq.com>
2017-07-14 11:20:27 +0800

    Update httpautotest.py

--- httpautotest/httpautotest.py
+++ httpautotest/httpautotest.py
@@ -27,6 +27,18 @@ s = requests.Session()
 s.mount('http://', HTTPAdapter(max_retries=3))
 s.mount('https://', HTTPAdapter(max_retries=3))
 
+'''
+logging.basicConfig(level=logging.DEBUG,
+                format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                datefmt='%a, %d %b %Y %H:%M:%S',
+                filename='myapp.log',
+                filemode='w')
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
+'''
 
 
 class myURLOpener(urllib.FancyURLopener):
