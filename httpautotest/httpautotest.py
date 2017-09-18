f8d1d0ea9f0d600def4cbe576071e90b58fc4dc5
andre.yang <717577064@qq.com>
2017-09-18 21:50:23 +0800

    change

--- httpautotest/httpautotest.py
+++ httpautotest/httpautotest.py
@@ -181,13 +181,11 @@ class httpautotest():
         self._checkdb(db['host'], db['db'], db['user'], db['passwd'], db['port'], excelurl, sheetname, rownum)
         return res
 
-    def testcase_one(self, domain, sheetname, excelurl, rownum, db, *args):
+    def testcase_one(self, domain, sheetname, excelurl, rownum, *args):
         do = self._getexcelparas(sheetname, excelurl, rownum)[1]
         remethod = self._getexcelparas(sheetname, excelurl, rownum)[2]
         payload = self._getexcelparas(sheetname, excelurl, rownum)[3]
         res = self._getres(domain, remethod, payload, do, *args)
-        db = self.todict(db)
-        self._checkdb(db['host'], db['db'], db['user'], db['passwd'], db['port'], excelurl, sheetname, rownum)
         return res
 
     def to_json(self, content, pretty_print=False):
