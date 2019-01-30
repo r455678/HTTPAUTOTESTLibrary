4dadb37b7b2c1ba35370b951df20876df4942821
r455678 <717577064@qq.com>
2019-01-30 17:02:21 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,5 +25,5 @@ setup(
     packages = find_packages(),
     include_package_data = True,
     platforms = "any",
-    install_requires = [requests,xlrd,pymysql]
+    install_requires = ['requests','xlrd','pymysql']
 )
